01592714215b35a4310e6adbca906fafc5fee567
Zaynah <zaynah.sadiq@hotmail.com>
2023-05-23 18:04:15 +0100

    connected API to react

--- src/containers/CharacterContainer.js
+++ src/containers/CharacterContainer.js
@@ -1,17 +1,32 @@
 import CharacterList from "../components/CharacterList";
 import CharacterForm from "../components/CharacterForm";
+import { useState, useEffect } from "react";
 
 
 const CharacterContainer = () => {
 
+    const [characters, setCharacters] = useState([])
 
+
+    const fetchCharacters = async () => {
+        const response = await fetch ("https://hp-api.onrender.com/api/characters/students")
+        const jsonData = await response.json();
+
+        setCharacters(jsonData);
+    }
+
+    useEffect(() => {
+        fetchCharacters();
+    }, [])
+    
     return(
     <>
         <h1>Hello from Character Container!</h1>
         <CharacterForm/>
         <CharacterList/>
     </>
-);
+    );
 }
+
  
 export default CharacterContainer;

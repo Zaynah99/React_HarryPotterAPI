c190fb09e49f30c5972aed52b2f0db5d390cc745
Zaynah <zaynah.sadiq@hotmail.com>
2023-05-24 10:43:31 +0100

    added character image and patronus

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,6 +3,8 @@ const  Character= ({character}) => {
         <>
         <h3> Name: {character.name}</h3>
         <p> House: {character.house}</p>
+        <p>Patronus:{character.patronus}</p>
+        <img src = {character.image}/> 
         </>
     );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -7,7 +7,6 @@ const CharacterList = ({ characters }) => {
         <Character 
         key={character.name}
         character={character}
-        image={character.image} 
         />
     );
  });
